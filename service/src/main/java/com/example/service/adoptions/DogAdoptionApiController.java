ed78dbf6d7b6033a35546eec022ef6aad8a0a6b4
Josh Long <josh@joshlong.com>
2025-03-03 00:06:48 -0800

    grpc's workin'

--- service/src/main/java/com/example/service/adoptions/DogAdoptionApiController.java
+++ service/src/main/java/com/example/service/adoptions/DogAdoptionApiController.java
@@ -8,7 +8,6 @@ import io.grpc.stub.StreamObserver;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
