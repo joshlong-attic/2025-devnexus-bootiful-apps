fbf6109752cd86a663355e96dd75e63b7f31363b
Josh Long <josh@joshlong.com>
2025-03-03 18:40:56 -0800

    polish

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -9,32 +9,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.URI;
+import java.util.Map;
 
 @SpringBootApplication
 public class ClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
 
-	// todo setup a spring cloud gateway app
+    // todo setup a spring cloud gateway app
 
-	@Bean
-	SecurityFilterChain mySecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity
-				.authorizeHttpRequests(a -> a.anyRequest().authenticated())
-				.csrf(AbstractHttpConfigurer::disable)
-				.cors(AbstractHttpConfigurer::disable)
-				.oauth2Login(Customizer.withDefaults())
-				.oauth2Client(Customizer.withDefaults())
-				.build();
-	}
+    @Bean
+    SecurityFilterChain mySecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(a -> a.anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .build();
+    }
 
 
 }
@@ -43,12 +47,17 @@ public class ClientApplication {
 @ResponseBody
 class ProxyController {
 
-	@GetMapping( "/**" )
-	ResponseEntity<?> ui(ProxyExchange<?> request) {
-		var path = request.path( "/"); // get everything after '/'
-		return request
-				.uri(URI.create(  "http://localhost:8080/" + path))
-				.get();
-	}
+    @GetMapping("/token")
+    Map<String,String> token(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient oAuth2AuthorizedClient) {
+        return Map.of("token", oAuth2AuthorizedClient.getAccessToken().getTokenValue());
+    }
+
+    @GetMapping("/**")
+    ResponseEntity<?> ui(ProxyExchange<?> request) {
+        var path = request.path("/"); // get everything after '/'
+        return request
+                .uri(URI.create("http://localhost:8080/" + path))
+                .get();
+    }
 
 }
